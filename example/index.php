c3b62077ea12526ee215e03d155fcfc67d98c9de
carry0987 <carry0987@users.noreply.github.com>
2024-01-19 14:19:19 +0800

    Update index.php

--- example/index.php
+++ example/index.php
@@ -20,7 +20,7 @@ echo 'Array from String Tags: ';
 print_r($arrayFromString);
 
 // Clear special characters in tag name
-$clearTagName = $tag->clearTagName('  Example Tag Name 2023  ');
+$clearTagName = Tag::clearTagName('  Example Tag Name 2023  ');
 echo 'Cleared Tag Name: ' . $clearTagName . PHP_EOL;
 
 // Let's say we have an array of tag IDs
@@ -31,20 +31,20 @@ $tagsArray = [
 ];
 
 // Merge tag IDs to a string
-$mergedTagIds = $tag->mergeTagID($tagsArray, 'id');
+$mergedTagIds = Tag::mergeTagID($tagsArray, 'id');
 echo 'Merged Tag IDs: ' . $mergedTagIds . PHP_EOL;
 
 // Split tag IDs to array based on string
-$slicedTagIds = $tag->sliceTagID('1,2,3,,');
+$slicedTagIds = Tag::sliceTagID('1,2,3,,');
 echo 'Sliced Tag IDs: ';
 print_r($slicedTagIds);
 
 // Check if specific tag ID exists in string
-$doesExist = $tag->checkTagExist('1,2,3', 2);
+$doesExist = Tag::checkTagExist('1,2,3', 2);
 echo 'Does tag ID 2 exist in the list? ' . ($doesExist ? 'Yes' : 'No') . PHP_EOL;
 
 // Remove a tag ID from string
-$removedTag = $tag->removeTag('1,2,3', 2);
+$removedTag = Tag::removeTag('1,2,3', 2);
 echo 'Tags after removing ID 2: ' . $removedTag . PHP_EOL;
 
 echo '</pre>';
